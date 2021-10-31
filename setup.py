343f2b0a07aec2b6eed1452eb0d8cc1a1f5666b5
Robert Komar <robkomar98@gmail.com>
2021-10-31 19:48:17 +0100

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Age classification based on speech recordings',
+    author='Robert Komar',
+    license='MIT',
+)
