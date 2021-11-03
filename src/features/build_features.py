4670c143b9a91b19f8c5a4d9721b3b431bf54eaa
Robert Komar <robkomar98@gmail.com>
2021-11-03 01:53:41 +0100

    un-nesting lists and adding columns with statistics

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,13 +4,29 @@ import json
 
 
 def load_interim_dataset(file_path=r"D:\PROJEKTY\intonat.dl\data\interim\data.json"):
-    with open(file_path) as fp:
-        pass
+    with open(file_path, 'r') as fp:
+        dataset_json = json.load(fp)
+        dataset_json.pop("fundamental_frequency")
+        return pd.DataFrame.from_dict(dataset_json)
+
+
+def prepare_datasets(data, features):
+    df = data.drop(["name", "sr"], axis=1)
+    df = df.set_index(['age']).apply(pd.Series.explode).reset_index()
+    statistics = [np.mean, np.std]
+    for feature in features:
+        for statistic in statistics:
+            df[f"{feature}_{str(str(np.std).split(' ')[1])}"] = df[feature].map(lambda x: statistic(x))
+    y = df["age"]
+    X = df.drop("age", axis=1)
+    print(df.head())
+    return X, y
 
 
 def main():
-    pass
+    data = load_interim_dataset()
+    X, y = prepare_datasets(data, ["mfcc, delta_mfcc, delta2_mfcc, hnr"])
 
 
 if __name__ == "__main__":
-    pass
+    main()
